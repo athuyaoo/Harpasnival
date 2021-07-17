63e5172a936c60c6edb530d6a23032fbd125fb9c
Aung Thuya Oo <thuyaoo200@gmail.com>
2021-07-18 01:17:48 +0800

    Add constants and utility files

---
+++ misc/constants.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# to share enums with the rest
+enum ColorType {BLUE, RED, PURPLE}
+const DirectionAngle = {
+	LOWER_RIGHT = 45,
+	RIGHT = 0,
+	UPPER_RIGHT = -45,
+	LOWER_LEFT = 135,
+	LEFT = -180,
+	UPPER_LEFT = -135
+}
---
+++ misc/utils.gd
@@ -0,0 +1,13 @@
+class_name Utils
+extends Node
+
+
+static func set_position_to_tile_map(current_position: Vector2):
+	var TILEMAP_SIZE = 64
+	var new_position = Vector2.ZERO
+	var offset = Vector2.ONE * (TILEMAP_SIZE / 2)
+	new_position = ((current_position - offset) / (TILEMAP_SIZE)).round() \
+				* (TILEMAP_SIZE) + offset
+	return new_position
+
+
