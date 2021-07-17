c05b3e7a0e4eeab8eafcf320aa214fe2efa95334
Aung Thuya Oo <thuyaoo200@gmail.com>
2021-07-18 01:16:23 +0800

    Update ball
    Add animation and let ball be freed when colliding with structure

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,5 +1,24 @@
+class_name Ball
 extends RigidBody2D
 
-func set_color(color:Color):
-	print(color)
-	$Sprite.modulate = color
+const ColorType = Constants.ColorType
+export(Color, RGB) var blue_ball_color
+export(Color, RGB) var red_ball_color
+export(Color, RGB) var purple_ball_color
+
+var current_color
+
+onready var colorDict = {
+	ColorType.BLUE: blue_ball_color,
+	ColorType.RED: red_ball_color,
+	ColorType.PURPLE: purple_ball_color,
+}
+
+func set_color(color):
+	if (color == null or colorDict[color] == null):
+		return
+	$Sprite.modulate = colorDict[color]
+	current_color = color
+
+func _on_Ball_body_entered(body):
+	queue_free()
