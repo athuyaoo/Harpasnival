9bd22d52b98cbfd7e0d6b968ce8a710dc582a295
Aung Thuya Oo <thuyaoo200@gmail.com>
2021-08-01 00:56:58 +0800

    Allow pedestal to be activated by hoops

--- pedestal/pedestal.gd
+++ pedestal/pedestal.gd
@@ -1,17 +1,18 @@
 extends Area2D
 
-
 signal activation_changed(is_activated)
 
-func _on_Pedestal_body_entered(body: Player) -> void:
+func _on_Pedestal_body_entered(body: PhysicsBody2D) -> void:
 	if not body:
 		return
 	$Sprite.frame = 1
 	emit_signal("activation_changed", true)
+	$AudioStreamPlayer.play()
 
 
-func _on_Pedestal_body_exited(body: Player) -> void:
+func _on_Pedestal_body_exited(body: PhysicsBody2D) -> void:
 	if not body:
 		return
 	$Sprite.frame = 0 # Replace with function body.
 	emit_signal("activation_changed", false)
+
