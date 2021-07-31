23f55e870ba1c505af3f2fe843c16f8a78726adc
Aung Thuya Oo <thuyaoo200@gmail.com>
2021-07-31 22:06:49 +0800

    Allow ball to reset

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,6 +1,8 @@
 class_name Ball
 extends RigidBody2D
 
+signal self_destructed
+
 const ColorType = Constants.ColorType
 export(Color, RGB) var blue_ball_color
 export(Color, RGB) var red_ball_color
@@ -9,6 +11,9 @@ export(Color, RGB) var purple_ball_color
 var start_position: Vector2
 var active = true
 var current_color
+var is_self_destructing = false
+
+const Level := preload("res://level design/level.gd")
 
 
 func set_start_position( player_position: Vector2):
@@ -22,14 +27,28 @@ onready var colorDict = {
 	ColorType.PURPLE: purple_ball_color,
 }
 
+func _ready() -> void:
+	var level = get_parent() as Level
+	if level == null:
+		return
+
+	connect("self_destructed", level, "on_ball_self_destructed",  [], CONNECT_DEFERRED)
+	print(self, "ready!")
+
 func set_active():
 	active = true
+
+func self_destruct():
+	is_self_destructing = true
+	print("self_destruct", self)
+	emit_signal("self_destructed")
+	queue_free()
 
 func _physics_process(delta):
 	if active:
 		return
-	if abs(global_position.x - start_position.x) > (64 * 3 + 10):
-		queue_free()
+	if abs(global_position.x - start_position.x) > (64 * 3 + 5):
+		self_destruct()
 
 
 func set_color(color):
@@ -39,4 +58,4 @@ func set_color(color):
 	current_color = color
 
 func _on_Ball_body_entered(body):
-	queue_free()
+	self_destruct()
