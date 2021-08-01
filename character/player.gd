da8258550be9e1f462a3503cc959d60be8091821
Aung Thuya Oo <thuyaoo200@gmail.com>
2021-08-02 00:13:04 +0800

    Fix bug to allow for bottom corner placement

--- character/player.gd
+++ character/player.gd
@@ -13,6 +13,7 @@ const placedown_sound = preload("res://sfx/placement.wav")
 
 onready var throw_direction := $Direction/ThrowPosition
 onready var detect_hoop_raycast := $Direction/DetectHoop
+onready var detect_underneath := $Direction/DetectUnderneath
 onready var placed_position := $Direction/PlacePosition
 onready var animation_player := $AnimationPlayer as AnimationPlayer
 var velocity = Vector2.ZERO
@@ -55,27 +56,35 @@ func move():
 
 func pick_up():
 	var detected_hoop := detect_hoop_raycast.get_collider() as Hoop
+	print(not detected_hoop or not detected_hoop.can_pick_up
+			or detected_hoop.is_anything_above() or is_holding_item())
 	if (not detected_hoop or not detected_hoop.can_pick_up
 			or detected_hoop.is_anything_above() or is_holding_item()):
 		return
-	if scene_tree.get_nodes_in_group("balls").size() > 0:
+	if scene_tree.get_nodes_in_group("balls").size() > 0 or $OnGround.get_collider() == null:
 		return
 	detected_hoop.pick_up()
 	held_item = detected_hoop
 	detect_hoop_raycast.add_exception(detected_hoop)
 	detect_hoop_raycast.set_collision_mask_bit(0, true)
 	detect_hoop_raycast.force_raycast_update()
+	
+	
+	detect_underneath.add_exception(detected_hoop)
+	detect_underneath.force_raycast_update()
 	$PlaceSound.stream = pickup_sound
 	$PlaceSound.play()
-	update_held_item_position()
+	call_deferred("update_held_item_position")
 
 func place_down():
 	assert(is_holding_item())
-	if (held_item.can_place_down()):
+	if (held_item.can_place_down() and $OnGround.get_collider() != null):
 		held_item.place_down()
 		held_item = null
 		detect_hoop_raycast.clear_exceptions()
+		detect_underneath.clear_exceptions()
 		detect_hoop_raycast.set_collision_mask_bit(0, false)
+
 		$PlaceSound.stream = placedown_sound
 		$PlaceSound.play()
 
@@ -83,8 +92,13 @@ func update_held_item_position():
 	if (!held_item):
 		return
 
+	var underneath_point = detect_underneath.get_collision_point()
+	var underneath_collider = detect_underneath.get_collider()
+	var underneath_tilemap_pos = Utils.set_position_to_tile_map(underneath_point)
+	
 	var collision_point = detect_hoop_raycast.get_collision_point()
 	var collider = detect_hoop_raycast.get_collider()
+	
 
 	var colliding_structure_tilemap_pos = Utils.set_position_to_tile_map(collision_point)
 	var detect_hoop_tilemap_pos = Utils.set_position_to_tile_map(detect_hoop_raycast.global_position)
@@ -99,11 +113,26 @@ func update_held_item_position():
 	is_raycast_in_structure =  is_raycast_in_structure or \
 		colliding_structure_tilemap_pos == detect_hoop_tilemap_pos
 
-	var cannot_place = (collider == null) or is_raycast_in_structure or (collider is Area2D)
+	var underneath_can_place = (underneath_collider != null) and not (underneath_collider is Area2D)
+
+	var cannot_place = (collider == null) or\
+		is_raycast_in_structure or\
+		(collider is Area2D)
+
 
 	if cannot_place:
-		held_item.global_position = Utils.set_position_to_tile_map(placed_position.global_position)
+		if underneath_can_place:
+			if $OnGround.get_collider() != null:
+				print("underneath_can_place")
+				held_item.global_position = underneath_tilemap_pos + Vector2.UP * 64
+				return
+		if $OnGround.get_collider() == null:
+			held_item.global_position = Utils.set_position_to_tile_map(
+				placed_position.global_position) - Vector2.RIGHT * $Direction.scale.x *64
+		else:
+			held_item.global_position = Utils.set_position_to_tile_map(placed_position.global_position)
 	else:
+		print("can_place")
 		held_item.global_position = colliding_structure_tilemap_pos + Vector2.UP * 64
 
 func is_holding_item():
