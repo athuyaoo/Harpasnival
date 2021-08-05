cb657ce61abbd30c330d6ba5f87a9fdaf9562e93
Daniel <e0235108@u.nus.edu>
2021-08-05 22:11:29 +0800

    implemented placeholder mute and quit scripts

--- level/level.gd
+++ level/level.gd
@@ -3,6 +3,8 @@ extends Node2D
 export var next_scene : PackedScene = null
 onready var targets = get_tree().get_nodes_in_group("targets")
 var completed = false
+var is_muted = false
+const main_menu_path = "res://level/main menu.tscn"
 
 func _ready() -> void:
 	var hoops = get_tree().get_nodes_in_group("hoops")
@@ -37,6 +39,22 @@ func on_ball_self_destructed():
 		for target in targets:
 			target.is_destroyed = false
 
+func toggle_mute():
+	if (!is_muted):
+		var master_sound = AudioServer.get_bus_index("Master")
+		AudioServer.set_bus_mute(master_sound, true)
+	else:
+		var master_sound = AudioServer.get_bus_index("Master")
+		AudioServer.set_bus_mute(master_sound, false)
+	is_muted = !is_muted
+
+func return_to_main_menu():
+	get_tree().change_scene(main_menu_path)
+
 func _input(event):
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
+	elif event.is_action_pressed("mute"):
+		toggle_mute()
+	elif event.is_action_pressed("quit"):
+		return_to_main_menu()
