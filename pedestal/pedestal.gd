e002baed4f48c1c10b25b6efe6b79c885349c12c
Aung Thuya Oo <thuyaoo200@gmail.com>
2021-08-07 02:15:14 +0800

    QOL improvement to place pedestal

--- pedestal/pedestal.gd
+++ pedestal/pedestal.gd
@@ -1,3 +1,4 @@
+tool
 extends Area2D
 
 signal activation_changed(is_activated)
@@ -16,3 +17,6 @@ func _on_Pedestal_body_exited(body: PhysicsBody2D) -> void:
 	$Sprite.frame = 0 # Replace with function body.
 	emit_signal("activation_changed", false)
 
+func _process(delta: float) -> void:
+	if Engine.editor_hint:
+		global_position = Utils.set_position_to_tile_map(global_position)
